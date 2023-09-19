7280b18d1037ba59077a3da88b432dedce716096
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-09-19 15:12:11 +0700

    mode dark

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
