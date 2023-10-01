1ccffee0525470ae28ca19e8003a95b5e701943f
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-10-01 13:19:20 +0700

    toggle

--- src/components/theme/ToggleColorMode.js
+++ src/components/theme/ToggleColorMode.js
@@ -3,21 +3,36 @@ import React, { useContext } from "react";
 import { ColorModeContext } from "./ColorModeProvider";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
+import {
+  FormControlLabel,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+  Typography,
+  experimentalStyled,
+} from "@mui/material";
+import FeatherIcon from "feather-icons-react";
 
 function ToggleColorMode() {
   const { toggleColorMode, theme } = useContext(ColorModeContext);
 
   return (
-    <>
-      {theme.palette.mode} mode
-      <button onClick={toggleColorMode}>
-        {theme.palette.mode === "dark" ? (
-          <Brightness7Icon />
-        ) : (
-          <Brightness4Icon />
-        )}
-      </button>
-    </>
+    <ListItem button onClick={toggleColorMode}>
+      <ListItemIcon sx={{ ml: 0.7 }}>
+        <FeatherIcon
+          color="white"
+          icon={theme.palette.mode === "dark" ? "moon" : "sun"}
+          width="20"
+          height="20"
+        />
+      </ListItemIcon>
+      <ListItemText>
+        <Typography color="white">
+          Mode {theme.palette.mode === "dark" ? "Gelap" : "Terang"}
+        </Typography>
+      </ListItemText>
+    </ListItem>
   );
 }
 
